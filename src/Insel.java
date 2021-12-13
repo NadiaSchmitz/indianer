98bb33766d2733b2661184bf12df179dec5f8910
NadiaSchmitz <nadiiaschmitz@gmail.com>
2021-12-13 09:21:27 +0100

    13122021

--- src/Insel.java
+++ src/Insel.java
@@ -1,20 +1,28 @@
 import java.text.DecimalFormat;
+import Prog1Tools.IOTools;
 
 public class Insel {
 
 	public static void main(String[] args) {
 		
-		int betrag_verkauf = 24, zinssatz, jahr;
+		int betrag_verkauf = 24, zinssatz, jahr_a, jahr_e;
 		double betrag_heute = betrag_verkauf;
 		
 		DecimalFormat df = new DecimalFormat("###.##");
 		
-		for(jahr = 1628; jahr < 2021; jahr++) {
+		jahr_a = IOTools.readInteger("Geben Sie das Jahr des Verkaufs ein: ");
+		jahr_e = IOTools.readInteger("Geben Sie das aktuelle Jahr ein: ");
+		betrag_verkauf = IOTools.readInteger("Geben Sie den Verkaufsbetrag ein: ");
+		zinssatz = IOTools.readInteger("Geben Sie den Zinssatz ein: ");
+		
+		for(jahr_a = 1628; jahr_a <= jahr_e; jahr_a++) {
 			
 			betrag_heute = betrag_heute + betrag_heute * 2 / 100;
 			
 		}
 		System.out.println("Indianer hätte " + df.format(betrag_heute) + "$ bekommen.");
+		System.out.println("Kapital im Jahr " + jahr_a + " betrug " + betrag_verkauf + "$");
+		System.out.println("Zinsatz pro Jahr beträgt " + zinssatz + " Prozent");
 	}
 
 }
